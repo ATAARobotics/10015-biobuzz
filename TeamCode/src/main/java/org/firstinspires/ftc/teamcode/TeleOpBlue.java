6dce7cd567cbcd90b96b177a753749f89b8b4881
meejah <meejah@meejah.ca>
2025-09-15 19:05:02 -0600

    real op-mode back

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBlue.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp (BLUE)", group="Opmode")
+public class TeleOpBlue extends TeleOp
+{
+    public Alliance getAlliance() { return Alliance.BLUE; }
+}
