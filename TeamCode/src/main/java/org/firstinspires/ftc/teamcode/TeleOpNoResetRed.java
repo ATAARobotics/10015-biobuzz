6e2231ff0e0a9b1b6b771ddd4d2c3dc8b6423089
meejah <meejah@meejah.ca>
2025-11-27 23:16:37 -0700

    reset turret in no reset?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpNoResetRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpNoResetRed.java
@@ -19,7 +19,8 @@ public class TeleOpNoResetRed extends TeleOp {
         float y = prefs.getFloat("y", 0.0f);
         float h = prefs.getFloat("heading", 0.0f);
         drive.setPosition(new Pose2D(DistanceUnit.METER, x, y, AngleUnit.DEGREES, h));
-        float t = prefs.getFloat("turret", 0.0f);
-        turret.setAngleReset(t);
+        //float t = prefs.getFloat("turret", 0.0f);
+        //turret.setAngleReset(t);
+        turret.angleReset();
     }
 }
