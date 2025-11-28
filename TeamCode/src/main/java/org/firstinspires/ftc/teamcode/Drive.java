b55b401249d3e062e3faed9cff094a578f2edd90
meejah <meejah@meejah.ca>
2025-11-27 23:46:35 -0700

    i guess this works (red untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -357,11 +357,11 @@ public class Drive extends SubsystemBase {
         public void execute() {
             // Run wheels in POV mode: use the Right stick to go forward & strafe, the Left stick to rotate left & right.
             if (isRedAlliance) {
-                strafe = scaleInputs(-driver.getRightX());
-                forward = scaleInputs(-driver.getRightY());
+                strafe = scaleInputs(-driver.getRightY());
+                forward = scaleInputs(-driver.getRightX());
             } else {
-                strafe = scaleInputs(driver.getRightX());
-                forward = scaleInputs(driver.getRightY());
+                strafe = scaleInputs(driver.getRightY());
+                forward = scaleInputs(driver.getRightX());
             }
 
             double leftX = driver.getLeftX();
