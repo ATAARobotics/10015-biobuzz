d4660f1257287962c0a18c7af2003aa5e5c73851
AveryStein5 <avery.stein21@gmail.com>
2025-12-01 20:47:40 -0700

    Implemented auto-tracking turret into teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -11,9 +13,11 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagMetadata;
@@ -36,7 +40,7 @@ public abstract class TeleOp extends OpMode {
 
     // prototyping with some AprilTags, Sept 15
     AprilTagProcessor april_tags;
-    //VisionPortal portal;
+    VisionPortal portal;
 
     public enum Alliance {RED, BLUE}
     public abstract Alliance getAlliance();
@@ -63,14 +67,14 @@ public abstract class TeleOp extends OpMode {
                 .setDrawCubeProjection(true)
                 .build();
 
-        /*portal = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(WebcamName.class,"DubbleBubble Webcam"))
+        portal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class,"elp"))
                 .addProcessor(april_tags)
-                .setCameraResolution(new Size(640, 480))
-                //.setStreamFormat(VisionPortal.StreamFormat.YUY2)
+                .setCameraResolution(new Size(800, 600))
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
                 .setAutoStopLiveView(true)
                 .build();
-*/
+
         drive = new Drive(hardwareMap, isRedAlliance);
         turret = new Turret(hardwareMap);
         intake = new Intake(hardwareMap);
@@ -176,7 +180,19 @@ public abstract class TeleOp extends OpMode {
         telem.log("position-y", y);
         telem.log("position-heading", h);
         telem.logDrivers("Robot Position", "x = %4.2f, y = %4.2f, h = %4.2f", x, y, h);
-
+        List<AprilTagDetection> detections = april_tags.getDetections();
+        telem.logBoth("april-tags", detections.size());
+        for (AprilTagDetection tag : detections) {
+            if (tag.id == target.id){
+                //  drive.april_bearing = drive.getPosition().getHeading(AngleUnit.DEGREES) - tag.ftcPose.bearing;
+                telem.logBoth("april-tag-target", tag.ftcPose.range);
+                //range(distance)is in inches, maybe convert to centi
+                telem.logBoth("april-tag-bearing", tag.ftcPose.bearing);
+                distToAprilTag = tag.ftcPose.range;
+                turret.faceRobotAngle(tag.ftcPose.bearing + turret.currentTurretAngle);
+                telem.logBoth("april-tag-distance", distToAprilTag);
+            }
+        }
         telemetry.update();
         FtcDashboard.getInstance().sendTelemetryPacket(pack);
     }
