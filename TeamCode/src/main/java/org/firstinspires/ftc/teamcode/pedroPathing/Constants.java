9fb3c37e97b5c5a0e7cf29e3fea6ae18008f1b6c
meejah <meejah@meejah.ca>
2026-01-11 15:28:34 -0700

    follow pedropath tutorial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,8 +3,14 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
@@ -23,12 +29,23 @@ public class Constants {
         .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
         ;
 
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+        .forwardPodY(-169)
+        .strafePodX(22)
+        .distanceUnit(DistanceUnit.MM)
+        .hardwareMapName("pinpoint")
+        .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+        .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+        .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+        ;
+
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
             .pathConstraints(pathConstraints)
             .mecanumDrivetrain(driveConstants)
+            .pinpointLocalizer(localizerConstants)
             .build();
     }
 }
