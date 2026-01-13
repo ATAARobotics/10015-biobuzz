cde429cf9645eca74c09b2850b5d919238ebeb39
meejah <meejah@meejah.ca>
2026-01-12 17:35:38 -0700

    fix telem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HyperTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HyperTelemetry.java
@@ -31,7 +31,8 @@ public class HyperTelemetry {
 
     // log to FTCDashboard and/or log file
     public void log(String key, Object value) {
-        pack.put(key, value);
+        PanelsTelemetry.INSTANCE.getFtcTelemetry().addData(key, value);
+        //pack.put(key, value);
         RobotLog.ii("HyperDroid", "\"%s\", \"%s\"", key, value);
     }
 
