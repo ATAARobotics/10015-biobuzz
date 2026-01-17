2e3a68860dbccc5b144bb7ccb253c713850f9604
Tirth Shah <tirthsh@ataarobotics.ca>
2026-01-16 20:50:08 -0700

    Testing auto pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -5,9 +5,6 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
-import com.pedropathing.paths.PathChain;
-import com.pedropathing.util.Timer;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
@@ -21,16 +18,22 @@ public class AutoTest extends OpMode {
 
     public Follower follower;
     private final Pose startPose = new Pose(56, 10, Math.toRadians(90)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(72, 72, Math.toRadians(0));
-    private Path scorePreload;
+   // private final Pose scorePose = new Pose(72, 72, Math.toRadians(0));
+    private final Pose poseOne = new Pose (45,36, Math.toRadians(180));
+    private final Pose poseTwo = new Pose (12.4,36.7, Math.toRadians(180));
+    private Path pathOne;
+    private Path pathTwo;
     public void init(){
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startPose);
-        scorePreload = new Path(new BezierLine(startPose, scorePose));
-        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+        pathOne = new Path(new BezierLine(startPose, poseOne));
+        pathOne.setLinearHeadingInterpolation(startPose.getHeading(), poseOne.getHeading());
+        pathTwo = new Path(new BezierLine(poseOne, poseTwo));
+        pathTwo.setLinearHeadingInterpolation(poseOne.getHeading(), poseTwo.getHeading());
     }
     public void start(){
-        follower.followPath(scorePreload);
+        follower.followPath(pathOne);
+        follower.followPath(pathTwo);
     }
     public void loop(){
         follower.update();
