d092b51d260a0a3832e9951f672bc5e7f83bf325
meejah <meejah@meejah.ca>
2026-02-02 20:52:18 -0700

    missed base clase

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpNoReset.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+
+public abstract class TeleOpNoReset extends TeleOp {
+
+    @Override
+    public void start() {
+        runtime.reset();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(hardwareMap.appContext);
+        float x = prefs.getFloat("x", 0.0f);
+        float y = prefs.getFloat("y", 0.0f);
+        float h = prefs.getFloat("heading", 0.0f);
+        drive.setPosition(new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.DEGREES, h));
+        int spinTarget = prefs.getInt("spindex-target", 0);
+        float turretAngle = prefs.getFloat("turret", 0.0f);
+        turret.setServoAngle(turretAngle);
+
+        // TODO: this isn't working, it seems like the encoder isn't
+        // being reset to 0 but is changing somehow when we start
+        // teleop? So we just reset and hope it was in a correct
+        // position (otherwise, operator has to fix it)
+        ////spindexer.targetAngle = spinTarget;
+        spindexer.reset();
+    }
+
+}
