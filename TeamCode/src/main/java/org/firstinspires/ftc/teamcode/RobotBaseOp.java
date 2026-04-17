44b53cfcee03e53483b6df850cbca7f52cda4a7e
meejah <meejah@meejah.ca>
2026-04-16 21:04:47 -0600

    pulse-the-intake edge case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBaseOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBaseOp.java
@@ -161,7 +161,18 @@ public abstract class RobotBaseOp extends OpMode {
             if (spindexer.isFull()) {
                 state = InState.DONE;
             } else {
-                state = InState.FIRST_TWO;
+		// driver often "pulses" the intake trigger .. so if
+		// we've just before this gotten to "SPIN" or past
+		// state, and the intake gets pulsed, we need to
+		// actually start in THIRD.
+		//
+		// we can know this if the "front" slot is empty (or
+		// maybe similarly if the other two slots are full)
+		if (spindexer.artifactCount() == 2 && !spindexer.artifactInSlot()) {
+		    state = InState.THIRD;
+		} else {
+		    state = InState.FIRST_TWO;
+		}
                 intake.grab();
                 intake.fullPower();
                 spindexer.spinModeIndex();
