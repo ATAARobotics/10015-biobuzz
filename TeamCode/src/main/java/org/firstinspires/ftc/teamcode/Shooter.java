10ebea3dcd903696a62ed48cfe94290487a434b4
meejah <meejah@meejah.ca>
2026-04-27 20:38:05 -0600

    one one more rpm tweak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -46,7 +46,7 @@ public class Shooter extends SubsystemBase {
 
     double hoodSlope;
     public static double RPM_LOW = 2650;  //2550;//2450;
-    public static double RPM_HIGH = 2900;
+    public static double RPM_HIGH = 3200;//2900;
     public static double RPM_LOW_DIST = 73.2;
     public static double RPM_HIGH_DIST = 104.19;
     public static double HOOD_MAX = 0.9;
