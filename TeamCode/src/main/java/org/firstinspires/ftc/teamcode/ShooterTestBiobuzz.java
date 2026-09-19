1614b408fc6a31f6ffe298adbeaf6c7e1dabd367
vince <vincent.stein@gmail.com>
2026-09-19 09:25:18 -0600

    Changed ShooterTestBiobuzz opmode class name to match file name.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTestBiobuzz.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTestBiobuzz.java
@@ -1,10 +1,11 @@
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Shooter Test")
+@TeleOp(name = "Motor Test")
 public class ShooterTestBiobuzz extends LinearOpMode {
 
     @Override
@@ -12,13 +13,38 @@ public class ShooterTestBiobuzz extends LinearOpMode {
 
         DcMotor motor = hardwareMap.get(DcMotor.class, "shooterL");
 
+        double motorPower = 0.0;
+
+        boolean lastDpadUp = false;
+        boolean lastDpadDown = false;
+
         waitForStart();
 
         while (opModeIsActive()) {
-            motor.setPower(1.0);   // 100% power
+
+            // Increase power by 0.1 on each new D-pad up press
+            if (gamepad1.dpad_up && !lastDpadUp) {
+                motorPower += 0.1;
+            }
+
+            // Decrease power by 0.1 on each new D-pad down press
+            if (gamepad1.dpad_down && !lastDpadDown) {
+                motorPower -= 0.1;
+            }
+
+            // Clamp power between 0.0 and 1.0
+            motorPower = Math.max(0.0, Math.min(1.0, motorPower));
+
+            motor.setPower(motorPower);
+
+            telemetry.addData("Motor Power", "%.2f", motorPower);
+            telemetry.update();
+
+            // Remember current button states for next loop
+            lastDpadUp = gamepad1.dpad_up;
+            lastDpadDown = gamepad1.dpad_down;
         }
 
         motor.setPower(0);
     }
 }
-
