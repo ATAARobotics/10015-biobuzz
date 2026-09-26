62731c5b4f54cc67c3516cb6d6c946f7ef4efdf0
meejah <meejah@meejah.ca>
2026-09-25 19:34:17 -0600

    cleanup and copy PollenDetector code to main roboto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBaseOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBaseOp.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+import android.util.Size;
+
 import com.seattlesolvers.solverslib.command.CommandBase;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.command.button.Trigger;
@@ -12,12 +15,23 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.Circle;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ColorSpace;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
+import org.opencv.core.Scalar;
+
 import java.util.LinkedList;
 import java.util.List;
+
 
 @Configurable
 public abstract class RobotBaseOp extends OpMode {
@@ -32,6 +46,9 @@ public abstract class RobotBaseOp extends OpMode {
     double strafe;
     double forward;
     double turn;
+
+    ColorBlobLocatorProcessor colorLocator;
+    VisionPortal portal;
 
     public enum StartZone {NEAR, FAR}
     public enum Alliance {RED, BLUE}
@@ -79,6 +96,48 @@ public abstract class RobotBaseOp extends OpMode {
             hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
         }
 	loops = 0;
+
+	// camera and vision setup
+        colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(
+                        new ColorRange(
+                                ColorSpace.HSV,
+                                new Scalar(15, 150, 100),
+                                new Scalar(35, 255, 255)))   // Use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setRoi(ImageRegion.entireFrame())
+                .setDrawContours(true)   // Show contours on the Stream Preview
+                .setBoxFitColor(0)       // Disable the drawing of rectangles
+                .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
+                .setBlurSize(5)          // Smooth the transitions between different colors in image
+
+                // the following options have been added to fill in perimeter holes.
+                .setDilateSize(3)       // Expand blobs to fill any divots on the edges
+                .setErodeSize(7)        // Shrink blobs back to original size
+                .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+
+                .build();
+        /*
+         * Build a vision portal to run the Color Locator process.
+         *
+         *  - Add the colorLocator process created above.
+         *  - Set the desired video resolution.
+         *      Since a high resolution will not improve this process, choose a lower resolution
+         *      that is supported by your camera.  This will improve overall performance and reduce
+         *      latency.
+         *  - Choose your video source.  This may be
+         *      .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))  .....   for a webcam
+         *  or
+         *      .setCamera(BuiltinCameraDirection.BACK)    ... for a Phone Camera
+         */
+        portal = new VisionPortal.Builder()
+	    .addProcessor(colorLocator)
+	    .setCameraResolution(new Size(640, 480))
+	    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+	    .build();
+
+        telemetry.setMsTransmissionInterval(100);   // Speed up telemetry updates for debugging.
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
     }
 
     protected void readSensors() {
@@ -100,13 +159,13 @@ public abstract class RobotBaseOp extends OpMode {
 
     protected void logTelemetry() {
         HyperTelemetry telem = new HyperTelemetry(telemetry);
-        telem.log("elapsed", runtime.seconds());
+        //telem.log("elapsed", runtime.seconds());
         telem.log("time", time);
         telem.log("battery", battery.getVoltage());
         telem.log("alliance", getAlliance());
 
-        double fps = loops / runtime.seconds();
-        telem.logDrivers("average fps", fps);
+        //double fps = loops / runtime.seconds();
+        //telem.logDrivers("average fps", fps);
 
         drive.addTelemetry(telem);
 
@@ -115,7 +174,7 @@ public abstract class RobotBaseOp extends OpMode {
 
     @Override
     public void start() {
-        runtime.reset();
+        //runtime.reset();
         // this is the far-zone starting position, against the wall with robot facing "north" / away from audience
         drive.setPosition(new Pose2D(DistanceUnit.INCH, isRedAlliance() ? 94 - 7.179 : 47.25 + 7.179, 7.19, AngleUnit.DEGREES, 90));
         loops = 0;
@@ -136,8 +195,23 @@ public abstract class RobotBaseOp extends OpMode {
         strafe = driver.getRightX();
         forward = -driver.getRightY();
         turn = 0;
-        drive.drivebase.driveRobotCentric(strafe, forward, turn);
 
+	// vision stuff: look for yellow / pollen blobs
+	List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
+	
+	ColorBlobLocatorProcessor.Util.filterByCriteria(ColorBlobLocatorProcessor.BlobCriteria.BY_CIRCULARITY, 0.7, 1, blobs);  // filter out non-circles.
+
+	telemetry.addData("Pollen Count", blobs.size());
+	for (ColorBlobLocatorProcessor.Blob b: blobs) {
+	    Circle circleFit = b.getCircle();
+
+	    telemetry.addData("Pollen","x=%d  y=%d", (int) circleFit.getX(), (int) circleFit.getY());
+	}
+
+
+	// actually do the drive command for this loop
+        drive.drivebase.driveRobotCentric(strafe, forward, turn);
+	
         logTelemetry();
     }
 
